7cf8985734a2137e01a4092ff0fffc7c5cd973d7
Jan Nikl <nikljan@fjfi.cvut.cz>
2020-04-17 13:06:26 +0200

    Added possibility to get the initial norm from IterativeSolver.

--- linalg/solvers.cpp
+++ linalg/solvers.cpp
@@ -171,6 +171,8 @@ void SLISolver::Mult(const Vector &b, Vector &x) const
       nom0 = nom = Dot(r, r);
    }
 
+   init_norm = sqrt(nom0);
+
    if (print_level == 1)
       mfem::out << "   Iteration : " << setw(3) << 0 << "  (B r, r) = "
                 << nom << '\n';
@@ -314,6 +316,8 @@ void CGSolver::Mult(const Vector &b, Vector &x) const
    }
    nom0 = nom = Dot(d, r);
    MFEM_ASSERT(IsFinite(nom), "nom = " << nom);
+
+   init_norm = sqrt(abs(nom0));
 
    if (print_level == 1 || print_level == 3)
    {
@@ -573,6 +577,7 @@ void GMRESSolver::Mult(const Vector &b, Vector &x) const
    double beta = Norm(r);  // beta = ||r||
    MFEM_ASSERT(IsFinite(beta), "beta = " << beta);
 
+   init_norm = beta;
    final_norm = std::max(rel_tol*beta, abs_tol);
 
    if (beta <= final_norm)
@@ -726,6 +731,7 @@ void FGMRESSolver::Mult(const Vector &b, Vector &x) const
    double beta = Norm(r);  // beta = ||r||
    MFEM_ASSERT(IsFinite(beta), "beta = " << beta);
 
+   init_norm = beta;
    final_norm = std::max(rel_tol*beta, abs_tol);
 
    if (beta <= final_norm)
@@ -915,6 +921,8 @@ void BiCGSTABSolver::Mult(const Vector &b, Vector &x) const
       mfem::out << "   Iteration : " << setw(3) << 0
                 << "   ||r|| = " << resid << '\n';
 
+   init_norm = resid;
+
    tol_goal = std::max(resid*rel_tol, abs_tol);
 
    if (resid <= tol_goal)
@@ -1088,6 +1096,7 @@ void MINRESSolver::Mult(const Vector &b, Vector &x) const
    gamma0 = gamma1 = 1.;
    sigma0 = sigma1 = 0.;
 
+   init_norm = beta;
    norm_goal = std::max(rel_tol*eta, abs_tol);
 
    if (eta <= norm_goal)
@@ -1271,6 +1280,8 @@ void NewtonSolver::Mult(const Vector &b, Vector &x) const
 
    norm0 = norm = Norm(r);
    norm_goal = std::max(rel_tol*norm, abs_tol);
+
+   init_norm = norm0;
 
    prec->iterative_mode = false;
 
@@ -1531,6 +1542,7 @@ void SLBQPOptimizer::Mult(const Vector& xt, Vector& x) const
    const double smin = 0.1;
 
    const double tol = max(abs_tol, rel_tol*a);
+   init_norm = a;
 
    // *** Start bracketing phase of SLBQP ***
    if (print_level > 1)
